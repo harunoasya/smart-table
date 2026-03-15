d7dcd1386ed1474fd451784f827b9738e89697d0
harunoasya <harunoasya@yandex.ru>
2026-03-15 15:27:19 +0300

    Выполненная работа

--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,47 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(templateId => {
+        const beforeTemplate = cloneTemplate(templateId);
+        root[templateId] = beforeTemplate;
+        root.container.prepend(beforeTemplate.container); 
+    });
+
+    after.forEach(templateId => {
+        const afterTemplate = cloneTemplate(templateId);
+        root[templateId] = afterTemplate;
+        root.container.append(afterTemplate.container);
+    });
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
 
+    root.container.addEventListener("change", () => {
+        onAction()});
+
+    root.container.addEventListener("reset", () => {
+        setTimeout(() => {
+            onAction();
+        }, 500);
+    });
+
+    root.container.addEventListener("submit", (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    });
+
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+
+            Object.keys(item).forEach(key => {
+                if (row.elements[key]) {
+                       row.elements[key].textContent = item[key];
+                }
+            })
+
+            return row.container;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
